ffecf269b9db242bc6eb02bc93d2c0f70b32055f
kricsleo <kricsleo@163.com>
2025-04-11 22:18:37 +0800

    wip: add holidays data

---
+++ scripts/chinese-holidays.ts
@@ -0,0 +1,28 @@
+import type { ChineseDay } from '../src/utils/chinese-days'
+import fs from 'node:fs/promises'
+import { glob } from 'tinyglobby'
+
+interface RawChineseHoliday {
+  name: string
+  date: string
+  isOffDay: boolean
+}
+
+async function main() {
+  const files = await glob('./holiday-cn/202*.json', {
+    absolute: true,
+  })
+
+  const holidays: ChineseDay[] = []
+  for await (const file of files) {
+    const days: RawChineseHoliday[] = (await import(file)).default.days
+    for (const day of days) {
+      const name = day.isOffDay ? day.name : `${day.name}补班`
+      holidays.push([name, day.date, day.isOffDay])
+    }
+  }
+
+  await fs.writeFile('./src/chinese-holidays.json', JSON.stringify(holidays))
+}
+
+main()
